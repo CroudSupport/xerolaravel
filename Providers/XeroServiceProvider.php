f0ab7f75e6da9f18e6b0e838ea3c735176837ccc
John Main <john.main@croud.co.uk>
2016-08-09 14:14:03 +0100

    Extending the options

--- Providers/XeroServiceProvider.php
+++ Providers/XeroServiceProvider.php
@@ -47,12 +47,40 @@ class XeroServiceProvider extends ServiceProvider
            return new \XeroPHP\Models\Accounting\Invoice();
         });
 
+        $this->app->bind('XeroPayment', function(){
+           return new \XeroPHP\Models\Accounting\Payment();
+        });
+
         $this->app->bind('XeroInvoiceLine', function(){
             return new \XeroPHP\Models\Accounting\Invoice\LineItem();
         });
 
         $this->app->bind('XeroContact', function(){
             return new \XeroPHP\Models\Accounting\Contact();
+        });
+
+        $this->app->bind('XeroContactPerson', function(){
+            return new \XeroPHP\Models\Accounting\ContactPerson();
+        });
+
+        $this->app->bind('XeroContactGroup', function(){
+            return new \XeroPHP\Models\Accounting\ContactGroup();
+        });
+
+        $this->app->bind('XeroAddress', function(){
+            return new \XeroPHP\Models\Accounting\Address();
+        });
+
+        $this->app->bind('XeroPhone', function(){
+            return new \XeroPHP\Models\Accounting\Phone();
+        });
+
+        $this->app->bind('XeroCurrency', function(){
+            return new \XeroPHP\Models\Accounting\Currency();
+        });
+
+        $this->app->bind('XeroAccount', function(){
+            return new \XeroPHP\Models\Accounting\Account();
         });
 
         $this->app->bind('XeroBrandingTheme', function(){
